662b364af3cb85e007481cdf69cebd2f88c4b0b6
Jojo-225 <eoyotode@gmail.com>
2026-02-09 10:37:29 +0100

    modification des attributs

--- src/main/java/jpa/Customer.java
+++ src/main/java/jpa/Customer.java
@@ -4,11 +4,6 @@ import java.time.LocalDate;
 
 public class Customer {
     private Long id;
-    private String nom;
-    private String prenom;
-    private LocalDate dateNaissance;
-    private String email;
-    private String role;
-    private String motDePss;
+  
 
 }
--- src/main/java/jpa/User.java
+++ src/main/java/jpa/User.java
@@ -1,5 +1,12 @@
 package jpa;
 
+import java.time.LocalDate;
+
 public class User {
     private Long id;
+    private String nom;
+    private String prenom;
+    private LocalDate dateNaissance;
+    private String email;
+    private String motDePss;
 }
